8383d2b80a825daa08cf4c3747715aef97f7ed75
Revi <olliv@outlook.com>
2021-06-17 09:25:36 +0200

    format shoot

--- site/libs/shoot.js
+++ site/libs/shoot.js
@@ -1,19 +1,19 @@
-			$.fn.hasAttr = function(name) {  
-				return this.attr(name) !== undefined;
-			};
-			$(document).ready(function() {
-				$('.sabot').click(function(e) {
-					e.preventDefault();
-					var targuet = $(this).attr('data-some');
-					$('#extfrm').attr('action',targuet);
-					if($(this).hasAttr('data-val')) {
-						var argum = $(this).attr('data-val');
-						$('#extval').attr('value',argum);
-					}
-					if($(this).hasAttr('data-comp')) {
-						var comp = $(this).attr('data-comp');
-						$('#extcomp').attr('value',comp);
-					}
-					$('#extfrm').submit();
-				});
-			});
+$.fn.hasAttr = function (name) {
+  return this.attr(name) !== undefined
+}
+$(document).ready(function () {
+  $('.sabot').click(function (e) {
+    e.preventDefault()
+    var targuet = $(this).attr('data-some')
+    $('#extfrm').attr('action', targuet)
+    if ($(this).hasAttr('data-val')) {
+      var argum = $(this).attr('data-val')
+      $('#extval').attr('value', argum)
+    }
+    if ($(this).hasAttr('data-comp')) {
+      var comp = $(this).attr('data-comp')
+      $('#extcomp').attr('value', comp)
+    }
+    $('#extfrm').submit()
+  })
+})
