5f859bb2a79bbf0fdc908724c6727d27aedd990f
Nickothan <s1nicolasrg@gmail.com>
2021-06-22 13:43:24 -0500

    update

--- site/libs/template.js
+++ site/libs/template.js
@@ -1,3 +1,45 @@
-;(function () {
-  console.log('auto ejecutable')
-})()
+$(document).ready(function () {
+  $menu = $('.left-side-menu').find('ul').find('li')
+
+  $menu.find('ul ul').slideUp()
+
+  $menu.hover(
+    function () {
+      $(this).addClass('active')
+      $(this).children('ul').stop()
+      $(this).children('ul').slideDown()
+      if ($(this).hasClass('clicked')) {
+        $(this).children('ul').slideDown()
+      }
+    },
+    function () {
+      $(this).removeClass('active')
+      $(this).children('ul').stop()
+      $(this).children('ul').slideUp()
+    }
+  )
+
+  $(this)
+    .children('ul')
+    .children('li')
+    .click(function () {
+      if ($(this).hasClass('clicked')) {
+        $(this).removeClass('clicked')
+      } else {
+        $(this).addClass('clicked')
+      }
+      console.log('Click : ', $(this).hasClass('clicked'))
+    })
+
+  $('.button-menu-mobible').click(function () {})
+})
+/*$(document).ready(function () {
+  $('#button-menu-mobile').click(function () {
+    $('.left-side-menu').css({
+      width: '70px'
+    })
+    $('.content-page').css({
+      margin: '70px 0 0 100px'
+    })
+  })
+})*/
