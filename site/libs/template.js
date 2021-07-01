30198d8a234cf834095bd9b08e85fa3325d01553
Revi <olliv@outlook.com>
2021-07-01 14:15:19 +0200

    left side menu works on mobile, remove outline on buttons

--- site/libs/template.js
+++ site/libs/template.js
@@ -1,3 +1,10 @@
+const checktablet = window.matchMedia('(max-width: 767px)')
+let isTablet = window.matchMedia('(max-width: 767px)').matches
+
+checktablet.addEventListener('change', function (e) {
+  isTablet = e.matches
+})
+
 $(document).ready(function () {
   $('.datapicker').flatpickr({
     allowInput: true
@@ -12,31 +19,41 @@ function SlideDropdownsLeftSideInside() {
 
   $elements.find('.nav-second-level').slideUp()
 
+  function show() {
+    $(this).addClass('active')
+    $(this).children('.nav-second-level').stop().slideDown()
+  }
+
+  function hide() {
+    $(this).removeClass('active')
+    $(this).children('.nav-second-level').stop().slideUp()
+  }
+
   $elements.hover(
     function () {
-      $(this).addClass('active')
-      $(this).children('.nav-second-level').stop().slideDown()
+      show()
     },
     function () {
-      if (!$(this).hasClass('clicked')) {
-        $(this).removeClass('active')
-        $(this).children('.nav-second-level').stop().slideUp()
-      }
+      !$(this).hasClass('clicked') && hide()
     }
   )
 
   $elements.click(function () {
     if ($(this).hasClass('clicked')) {
       $(this).removeClass('clicked')
+      
+      isTablet && show()
     } else {
       $(this).addClass('clicked')
+
+      isTablet && hide()
     }
   })
 }
 
 function MenuBehavior() {
   $('.button-menu-mobile').click(function () {
-    $('body').toggleClass('enlarged')
+    $('body').toggleClass(isTablet ? ' sidebar-enable' : 'enlarged')
   })
 
   $('.right-bar-toggle').click(function () {
