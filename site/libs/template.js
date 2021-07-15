a578e688aae59aebc1a7b5c0e2816fe2e1ff0440
Nickothan <s1nicolasrg@gmail.com>
2021-07-15 06:45:31 -0500

    Se elimino la funcion dropdown

--- site/libs/template.js
+++ site/libs/template.js
@@ -1,70 +1,62 @@
-const checktablet = window.matchMedia("(max-width: 767px)");
-let isTablet = window.matchMedia("(max-width: 767px)").matches;
+const checktablet = window.matchMedia('(max-width: 767px)')
+let isTablet = window.matchMedia('(max-width: 767px)').matches
 
-checktablet.addEventListener("change", function (e) {
-  isTablet = e.matches;
-});
+checktablet.addEventListener('change', function (e) {
+  isTablet = e.matches
+})
 
 $(document).ready(function () {
-  $(".datapicker").flatpickr({
-    allowInput: true,
-  });
-  SlideDropdownsLeftSideInside();
+  $('.datapicker').flatpickr({
+    allowInput: true
+  })
+  SlideDropdownsLeftSideInside()
   // Right Side Menu
-  RightSideMenu();
-  dropdown();
-});
+  RightSideMenu()
+})
 
 function SlideDropdownsLeftSideInside() {
-  var $elements = $(".left-side-menu").find("ul li");
+  var $elements = $('.left-side-menu').find('ul li')
 
-  $elements.find(".nav-second-level").slideUp();
+  $elements.find('.nav-second-level').slideUp()
 
   function show($el) {
-    $el.addClass("active");
-    $el.children(".nav-second-level").stop().slideDown();
+    $el.addClass('active')
+    $el.children('.nav-second-level').stop().slideDown()
   }
 
   function hide($el) {
-    $el.removeClass("active");
-    $el.children(".nav-second-level").stop().slideUp();
+    $el.removeClass('active')
+    $el.children('.nav-second-level').stop().slideUp()
   }
 
   $elements.hover(
     function () {
-      show($(this));
+      show($(this))
     },
     function () {
-      !$(this).hasClass("clicked") && hide($(this));
+      !$(this).hasClass('clicked') && hide($(this))
     }
-  );
+  )
 
   $elements.click(function () {
-    if ($(this).hasClass("clicked")) {
-      $(this).removeClass("clicked");
+    if ($(this).hasClass('clicked')) {
+      $(this).removeClass('clicked')
 
-      hide($(this));
+      hide($(this))
     } else {
-      $(this).addClass("clicked");
+      $(this).addClass('clicked')
 
-      show($(this));
+      show($(this))
     }
-  });
+  })
 }
 
 function RightSideMenu() {
-  $(".button-menu-mobile").click(function () {
-    $("body").toggleClass(isTablet ? " sidebar-enable" : "enlarged");
-  });
+  $('.button-menu-mobile').click(function () {
+    $('body').toggleClass(isTablet ? ' sidebar-enable' : 'enlarged')
+  })
 
-  $(".right-bar-toggle").click(function () {
-    $("body").toggleClass("right-bar-enabled");
-  });
-}
-
-function dropdown() {
-  $(".dropdown").click(function () {
-    $(this).toggleClass("show");
-    $(this).children(".dropdown-menu").toggleClass("show");
-  });
+  $('.right-bar-toggle').click(function () {
+    $('body').toggleClass('right-bar-enabled')
+  })
 }
