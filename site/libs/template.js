3196c6af53f1d7add4666a5fce3234d7e7643454
Revi <oliiv@outlook.com>
2021-11-22 18:13:34 +0100

    Validation (#34)
    * Bootstrap form
    
    * caracters counter
    
    Co-authored-by: Nickothan <s1nicolasrg@gmail.com>

--- site/libs/template.js
+++ site/libs/template.js
@@ -1,81 +1,102 @@
-const checktablet = window.matchMedia("(max-width: 767px)")
-let isTablet = window.matchMedia("(max-width: 767px)").matches
+const checktablet = window.matchMedia("(max-width: 767px)");
+let isTablet = window.matchMedia("(max-width: 767px)").matches;
 
 checktablet.addEventListener("change", function (e) {
-  isTablet = e.matches
-})
+  isTablet = e.matches;
+});
 
 $(document).ready(function () {
   $(".datapicker").flatpickr({
     allowInput: true,
-    monthSelectorType: "static"
-  })
-  SlideDropdownsLeftSideInside()
-  // Right Side Menu
-  RightSideMenu()
-  validationForm()
-  handleDarkMode()
-})
+    monthSelectorType: "static",
+  });
+  SlideDropdownsLeftSideInside();
+  RightSideMenu();
+  handleDarkMode();
+  maxLength();
+  validationForm();
+});
 
 function SlideDropdownsLeftSideInside() {
-  var $elements = $(".left-side-menu").find("ul li")
+  var $elements = $(".left-side-menu").find("ul li");
 
-  $elements.find(".nav-second-level").slideUp()
+  $elements.find(".nav-second-level").slideUp();
 
   function show($el) {
-    $el.addClass("active")
-    $el.children(".nav-second-level").stop().slideDown()
+    $el.addClass("active");
+    $el.children(".nav-second-level").stop().slideDown();
   }
 
   function hide($el) {
-    $el.removeClass("active")
-    $el.children(".nav-second-level").stop().slideUp()
+    $el.removeClass("active");
+    $el.children(".nav-second-level").stop().slideUp();
   }
 
   $elements.hover(
     function () {
-      show($(this))
+      show($(this));
     },
     function () {
-      !$(this).hasClass("clicked") && hide($(this))
+      !$(this).hasClass("clicked") && hide($(this));
     }
-  )
+  );
 
   $elements.click(function () {
     if ($(this).hasClass("clicked")) {
-      $(this).removeClass("clicked")
+      $(this).removeClass("clicked");
 
-      hide($(this))
+      hide($(this));
     } else {
-      $(this).addClass("clicked")
+      $(this).addClass("clicked");
 
-      show($(this))
+      show($(this));
     }
-  })
+  });
 }
 
 function RightSideMenu() {
   $(".button-menu-mobile").click(function () {
-    $("body").toggleClass(isTablet ? " sidebar-enable" : "enlarged")
-  })
+    $("body").toggleClass(isTablet ? " sidebar-enable" : "enlarged");
+  });
 
   $(".right-bar-toggle, .rightbar-overlay").click(function () {
-    $("body").toggleClass("right-bar-enabled")
-  })
-}
-
-function validationForm() {
-  $(
-    "input#fldhost, input#fldsite, input#fldbase, input#fldrecsitekey, input#fldrecsecret, input#flduser, input#fldmailadm, input#fldmaincolor, input#fldseccolor"
-  ).characterCounter()
+    $("body").toggleClass("right-bar-enabled");
+  });
 }
 
 function handleDarkMode() {
   $("#chbfixedaddt").on("click", function () {
     if ($("#chbfixedaddt")[0].checked) {
-      $("body").addClass("dark-mode")
+      $("body").addClass("dark-mode");
     } else {
-      $("body").removeClass("dark-mode")
+      $("body").removeClass("dark-mode");
     }
-  })
+  });
+}
+
+function validationForm() {
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+}
+
+function maxLength() {
+  console.log(" it is working?");
+  $("input[maxlength], textarea[maxlength]").maxlength({
+    alwaysShow: true,
+  });
 }
