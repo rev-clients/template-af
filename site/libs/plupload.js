c9e8cb33a9c1d1a854ef6305d53c1cd5f3485754
Revi <oliiv@outlook.com>
2021-12-18 11:48:24 +0100

    new model

--- site/libs/plupload.js
+++ site/libs/plupload.js
@@ -1,26 +1,16 @@
 $(function () {
-  $("#uploader").plupload({
+  // Setup html5 version
+  $("#uploader").pluploadQueue({
     // General settings
     runtimes: "html5,flash,silverlight,html4",
-
-    url: "../upload.php", // aqui va el link
-
-    // User can upload no more then 20 files in one go (sets multiple_queues to false)
-    max_file_count: 20,
-
+    url: "../upload.php",
     chunk_size: "1mb",
-
-    // Resize images on clientside if we can
-    resize: {
-      width: 200,
-      height: 200,
-      quality: 90,
-      crop: true, // crop to exact dimensions
-    },
+    rename: true,
+    dragdrop: true,
 
     filters: {
       // Maximum file size
-      max_file_size: "1000mb",
+      max_file_size: "10mb",
       // Specify what files to browse for
       mime_types: [
         { title: "Image files", extensions: "jpg,gif,png" },
@@ -28,42 +18,10 @@ $(function () {
       ],
     },
 
-    // Rename files by clicking on their titles
-    rename: true,
+    // Resize images on clientside if we can
+    resize: { width: 320, height: 240, quality: 90 },
 
-    // Sort files
-    sortable: true,
-
-    // Enable ability to drag'n'drop files onto the widget (currently only HTML5 supports that)
-    dragdrop: true,
-
-    // Views to activate
-    views: {
-      list: true,
-      thumbs: true, // Show thumbs
-      active: "thumbs",
-    },
-
-    // Flash settings
     flash_swf_url: "../../js/Moxie.swf",
-
-    // Silverlight settings
     silverlight_xap_url: "../../js/Moxie.xap",
-  });
-
-  // Handle the case when form was submitted before uploading has finished
-  $("#form").submit(function (e) {
-    // Files in queue upload them first
-    if ($("#uploader").plupload("getFiles").length > 0) {
-      // When all files are uploaded submit form
-      $("#uploader").on("complete", function () {
-        $("#form")[0].submit();
-      });
-
-      $("#uploader").plupload("start");
-    } else {
-      alert("You must have at least one file in the queue.");
-    }
-    return false; // Keep the form from submitting
   });
 });
